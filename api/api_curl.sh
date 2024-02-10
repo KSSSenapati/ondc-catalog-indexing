9f3109246dab3af6a532a8fa2261f18c12f3aabe
arnabGudu <arnabgudu.gs@gmail.com>
2024-02-11 05:27:01 +0530

    Add optimized api

--- api/api_curl.sh
+++ api/api_curl.sh
@@ -4,14 +4,15 @@ input=$1
 
 if [[ $input == "addProduct" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productTitle": "Ponds"}' \
+    -d '{"product_title": "Ponds"}' \
     http://localhost:8000/addProduct
 fi
 
 
 if [[ $input == "queryProduct" ]]; then
-    curl -X PUT -H "Content-Type: application/json" \
-    http://localhost:8000/queryProduct/1
+    curl -X POST -H "Content-Type: application/json" \
+    -d '{"product_id": "123"}' \
+    http://localhost:8000/queryProduct
 fi
 
 
@@ -21,50 +22,50 @@ if [[ $input == "deleteProduct" ]]; then
 fi
 
 
-if [[ $input == "updateAttributes" ]]; then
+if [[ $input == "updateProduct" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productId": "1", "discount": "2"}' \
-    http://localhost:8000/updateAttributes
+    -d '{"product_id": "1234", "product_title": "ponds cream"}' \
+    http://localhost:8000/updateProduct
 fi
 
 
 if [[ $input == "updateDiscount" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productId": "1", "discount": "2"}' \
-    http://localhost:8000/updateDiscount
+    -d '{"product_id": 1, "discount": 2}' \
+    http://localhost:8000/updateProduct
 fi
 
 
 if [[ $input == "updateSKU" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productId": "1", "sizesCount": {"s": "2"}}' \
-    http://localhost:8000/updateSKU
+    -d '{"product_id": 1234, "sizesCount": {"s": 1, "m": 3}}' \
+    http://localhost:8000/updateProduct
 fi
 
 
 if [[ $input == "updateRating" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productId": "1", "rating": "4.4"}' \
-    http://localhost:8000/updateRating
+    -d '{"product_id": "1234", "rating": 4.4}' \
+    http://localhost:8000/updateProduct
 fi
 
 
 if [[ $input == "updateTag" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productId": "1", "acceleratorTag": "["new", "old"]"}' \
-    http://localhost:8000/updateTag
+    -d '{"product_id": 1, "accelerator_tag": ["new"]}' \
+    http://localhost:8000/updateProduct
 fi
 
 
 if [[ $input == "updateImage" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productId": "1", "image": "/path/img.jpg"}' \
-    http://localhost:8000/updateImage
+    -d '{"product_id": 1, "main_image": "/path/img.jpg"}' \
+    http://localhost:8000/updateProduct
 fi
 
 
 if [[ $input == "updateAd" ]]; then
     curl -X POST -H "Content-Type: application/json" \
-    -d '{"productId": "1", "ad": "0"}' \
-    http://localhost:8000/updateAd
+    -d '{"product_id": 1, "ad_enabled": 0}' \
+    http://localhost:8000/updateProduct
 fi
--- api/service.py
+++ api/service.py
@@ -12,25 +12,50 @@ def get_solr_params(message):
     doc = {}
     for message_key in message:
         if message_key == "product_id":
-            doc[message_key] = "doc_" + message[message_key]
+            doc['id'] = "doc_" + str(message[message_key])
         elif message_key == "sizesCount":
             updates["sizes_facet"] = "set"
             doc["sizes_facet"] = list(message[message_key])
             for size in list(message[message_key]):
                 updates[size + "_size_count"] = "set"
                 doc[size + "_size_count"] = message[message_key][size]
+        elif message_key == "attributes":
+            for attribute in list(message[message_key]):
+                updates[attribute + "_atsa"] = "set"
+                doc[attribute + "_atsa"] = message[message_key][attribute]
         else:
             updates[message_key] = "set"
             doc[message_key] = message[message_key]
+    print(updates, doc)
     return updates, doc
+
+def get_solr_params_to_add(message):
+    message = json.loads(message.value.decode('utf-8'))
+    doc = {}
+    for message_key in message:
+        if message_key == "product_id":
+            doc[message_key] = message[message_key]
+            doc['id'] = "doc_" + str(message[message_key])
+        elif message_key == "sizesCount":
+            doc["sizes_facet"] = list(message[message_key])
+            for size in list(message[message_key]):
+                doc[size + "_size_count"] = message[message_key][size]
+        elif message_key == "attributes":
+            for attribute in list(message[message_key]):
+                doc[attribute + "_atsa"] = message[message_key][attribute]
+        else:
+            doc[message_key] = message[message_key]
+    print(doc)
+    return doc
 
 def addProduct():
     topic = 'addProduct'
     consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
     for message in consumer:
         print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        updates, doc = get_solr_params(message)
-        solr.add([doc], fieldUpdates = updates)
+        doc = get_solr_params_to_add(message)
+        print(doc)
+        solr.add([doc])
 
 
 def deleteProduct():
@@ -38,66 +63,12 @@ def deleteProduct():
     consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
     for message in consumer:
         print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        doc_id = "doc_" + json.loads(message.value.decode('utf-8'))["product_id"]
+        doc_id = "doc_" + str(json.loads(message.value.decode('utf-8'))["product_id"])
         solr.delete(id = doc_id)
 
 
-def updateAttributes():
-    topic = 'updateAttributes'
-    consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
-    for message in consumer:
-        print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        updates, doc = get_solr_params(message)
-        solr.add([doc], fieldUpdates = updates)
-
-
-def updateDiscount():
-    topic = 'updateDiscount'
-    consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
-    for message in consumer:
-        print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        updates, doc = get_solr_params(message)
-        solr.add([doc], fieldUpdates = updates)
-
-
-def updateSKU():
-    topic = 'updateSKU'
-    consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
-    for message in consumer:
-        print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        updates, doc = get_solr_params(message)
-        solr.add([doc], fieldUpdates = updates)
-
-
-def updateRating():
-    topic = 'updateRating'
-    consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
-    for message in consumer:
-        print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        updates, doc = get_solr_params(message)
-        solr.add([doc], fieldUpdates = updates)
-
-
-def updateTag():
-    topic = 'updateTag'
-    consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
-    for message in consumer:
-        print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        updates, doc = get_solr_params(message)
-        solr.add([doc], fieldUpdates = updates)
-
-
-def updateImage():
-    topic = 'updateImage'
-    consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
-    for message in consumer:
-        print(f"Received message in {topic}: {message.value.decode('utf-8')}")
-        updates, doc = get_solr_params(message)
-        solr.add([doc], fieldUpdates = updates)
-
-
-def updateAd():
-    topic = 'updateAd'
+def updateProduct(topic):
+    topic = 'updateProduct'
     consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'])
     for message in consumer:
         print(f"Received message in {topic}: {message.value.decode('utf-8')}")
@@ -106,21 +77,14 @@ def updateAd():
 
 
 if __name__ == "__main__":
-    # Create and start a thread for each consumer function
-    threads = [
-        threading.Thread(target=addProduct),
-        threading.Thread(target=deleteProduct),
-        threading.Thread(target=updateAttributes),
-        threading.Thread(target=updateDiscount),
-        threading.Thread(target=updateSKU),
-        threading.Thread(target=updateRating),
-        threading.Thread(target=updateTag),
-        threading.Thread(target=updateImage),
-        threading.Thread(target=updateAd)
-    ]
+    topics = ['addProduct', 'deleteProduct', 'updateProduct', 'updateDiscount', 
+              'updateSKU', 'updateRating', 'updateTag', 'updateImage', 'updateAd']
+
+    threads = [threading.Thread(target=updateProduct, args=(topic,)) for topic in topics]
+    threads += [threading.Thread(target=addProduct), threading.Thread(target=deleteProduct)]
+
     for thread in threads:
         thread.start()
 
-    # Wait for all threads to complete
     for thread in threads:
         thread.join()
