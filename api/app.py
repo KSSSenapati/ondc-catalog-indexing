a9c6399f85711d01228884cb56aeae848d701aeb
Arnab Paikaray <42674892+arnabGudu@users.noreply.github.com>
2024-02-11 05:29:00 +0530

    Add optimized api (#9)

--- api/app.py
+++ api/app.py
@@ -16,6 +16,7 @@ solr = pysolr.Solr('http://localhost:8983/solr/ondc', timeout=10)
 client = pymongo.MongoClient(host="localhost", port=27017, username="admin", password="admin")
 db = client["lookup"]
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -25,57 +26,63 @@ app.add_middleware(
 )
 
 
+def image_handler(message):
+    collection = db["image"]
+    if message.get("main_image") is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="product_id is required")
+    document = {
+        "product_id": message["product_id"], "image_path": message["main_image"]}
+    result = collection.insert_one(document)
+    return str(result.inserted_id)
+
+
+kafka_map = {
+    'updateDiscount': {'fields': ['discount', 'sale_discount'], 'handler': None},
+    'updateSKU': {'fields': ['sizesCount'], 'handler': None},
+    'updateRating': {'fields': ['rating'], 'handler': None},
+    'updateTag': {'fields': ['accelerator_tag'], 'handler': None},
+    'updateAd': {'fields': ['ad_enabled'], 'handler': None},
+    'updateImage': {'fields': ['main_image'], 'handler': image_handler},
+    'updateProduct': {'fields': ['product_title', 'master_category', 'sub_category',
+        'article', 'attribute', 'price', 'pincode'], 'handler': None},
+}
+
+
 @app.post("/addProduct", status_code=status.HTTP_200_OK)
 async def addProduct(request: Request):
     try:
         body = await request.json()
-        message = {
-            "productTitle": body.get("productTitle"),
-            "masterCategory": body.get("masterCategory"),
-            "subCategory": body.get("subCategory"),
-            "article": body.get("article"),
-            "attribute": body.get("getAttributes"),
-            "price": body.get("price"),
-            "pinCode": body.get("pinCode"),
-            "discount": body.get("discount"),
-            "sizesCount": body.get("sizesCount"),
-            "rating": body.get("rating"),
-            "acceleratorTag": body.get("acceleratorTag"),
-            "image": body.get("image"),
-            "ad": body.get("ad"),
-        }
 
-        if body.get("productTitle") is None:
+        if body.get('product_id') is None:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="productId is required")
+                status_code=status.HTTP_400_BAD_REQUEST, detail="product_id is required")
 
-        keys_to_remove = [key for key, val in message.items() if val is None]
-        for key in keys_to_remove:
-            message.pop(key)
-
-        collection = db["product"]
-        document = {"productTitle": message["productTitle"]}
-        result = collection.insert_one(document)
-        message["productId"] = str(result.inserted_id)
-
-        producer.send('addProduct', value=json.dumps(message).encode('utf-8'))
+        body['product_id'] = '1234'
+        producer.send('addProduct', value=json.dumps(body).encode('utf-8'))
         producer.flush()
-        return {'status': 'success', 'message': 'New product added successfully'}
+        return {'status': 'success', 'message': 'Add successful'}
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@app.put("/queryProduct/{id}", status_code=status.HTTP_200_OK)
-async def queryProduct(id: str, content: str):
+@app.post("/queryProduct", status_code=status.HTTP_200_OK)
+async def queryProduct(request: Request):
     try:
-        if not id:
+        body = await request.json()
+        if body.get('product_id') is None:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="productId is required")
+                status_code=status.HTTP_400_BAD_REQUEST, detail="product_id is required")
 
-        query = 'id:"doc_{}"'.format(id)
+        query = 'id:"doc_{}"'.format(body.get('product_id'))
         results = solr.search(query)
-        return {'status': 'success', 'message': results}
+        if len(results.docs):
+            docs = results.docs[0]
+        else:
+            raise raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Document not found")
+        return {'status': 'success', 'message': docs}
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
@@ -86,194 +93,34 @@ async def deleteProduct(id: str):
     try:
         if not id:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="productId is required")
+                status_code=status.HTTP_400_BAD_REQUEST, detail="product_id is required")
 
         producer.send('deleteProduct', key=id.encode())
         producer.flush()
-        return {'status': 'success', 'message': 'Product deleted successfully'}
+        return {'status': 'success', 'message': 'Delete successful'}
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@app.post("/updateAttributes", status_code=status.HTTP_200_OK)
-async def updateAttributes(request: Request):
+@app.post("/updateProduct", status_code=status.HTTP_200_OK)
+async def updateProduct(request: Request):
     try:
         body = await request.json()
-        api_list = [updateDiscount, updateSKU,
-                    updateRating, updateTag, updateImage, updateAd]
-        for api in api_list:
-            try:
-                api(request)
-            except Exception as e:
-                print(f"Exception in {api.__name__}: {e}")
-                pass
 
-        message = {
-            "productId": body.get("productId"),
-            "productTitle": body.get("productTitle"),
-            "masterCategory": body.get("masterCategory"),
-            "subCategory": body.get("subCategory"),
-            "article": body.get("article"),
-            "attribute": body.get("getAttributes"),
-            "price": body.get("price"),
-            "pinCode": body.get("pinCode"),
-        }
-
-        keys_to_remove = [key for key, val in message.items() if val is None]
-        for key in keys_to_remove:
-            message.pop(key)
-
-        producer.send('updateAttributes', value=json.dumps(
-            message).encode('utf-8'))
-        producer.flush()
-        return {'status': 'success', 'message': 'Attributes updated successfully'}
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@app.post("/updateDiscount", status_code=status.HTTP_200_OK)
-async def updateDiscount(request: Request):
-    try:
-        body = await request.json()
-        message = {
-            "productId": body.get("productId"),
-            "discount": body.get("discount"),
-            "salesDiscount": body.get("salesDiscount"),
-        }
-
-        if message["salesDiscount"] is None:
-            message["salesDiscount"] = 0
-        print(message)
-        for key, val in message.items():
-            if val is None:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, detail=f"{key} is required")
-
-        producer.send('updateDiscount', value=json.dumps(
-            message).encode('utf-8'))
-        producer.flush()
-        return {'status': 'success', 'message': 'Discount updated successfully'}
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@app.post("/updateSKU", status_code=status.HTTP_200_OK)
-async def updateSKU(request: Request):
-    try:
-        body = await request.json()
-        message = {
-            "productId": body.get("productId"),
-            "sizesCount": body.get("sizesCount"),
-        }
-
-        for key, val in message.items():
-            if val is None:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, detail=f"{key} is required")
-
-        producer.send('updateSKU', value=json.dumps(message).encode('utf-8'))
-        producer.flush()
-        return {'status': 'success', 'message': 'SKU updated successfully'}
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@app.post("/updateRating", status_code=status.HTTP_200_OK)
-async def updateRating(request: Request):
-    try:
-        body = await request.json()
-        message = {
-            "productId": body.get("productId"),
-            "rating": body.get("rating"),
-        }
-
-        for key, val in message.items():
-            if val is None:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, detail=f"{key} is required")
-
-        producer.send('updateRating', value=json.dumps(
-            message).encode('utf-8'))
-        producer.flush()
-        return {'status': 'success', 'message': 'Data pushed to Kafka successfully.'}
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@app.post("/updateTag", status_code=status.HTTP_200_OK)
-async def updateTag(request: Request):
-    try:
-        body = await request.json()
-        message = {
-            "productId": body.get("productId"),
-            "acceleratorTag": body.get("acceleratorTag"),
-        }
-
-        for key, val in message.items():
-            if val is None:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, detail=f"{key} is required")
-
-        producer.send('updateTag', value=json.dumps(message).encode('utf-8'))
-        producer.flush()
-        return {'status': 'success', 'message': 'Tag updated successfully'}
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@app.post("/updateImage", status_code=status.HTTP_200_OK)
-async def updateImage(request: Request):
-    try:
-        body = await request.json()
-        message = {
-            "productId": body.get("productId"),
-            "image": body.get("image"),
-        }
-
-        for key, val in message.items():
-            if val is None:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, detail=f"{key} is required")
-
-        collection = db["image"]
-        document = {
-            "productId": message["productId"], "imagePath": message["image"]}
-        result = collection.insert_one(document)
-
-        message["imageId"] = str(result.inserted_id)
-        message.pop('image')
-
-        producer.send('updateImage', value=json.dumps(message).encode('utf-8'))
-        producer.flush()
-        return {'status': 'success', 'message': 'Image updated successfully'}
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@app.post("/updateAd", status_code=status.HTTP_200_OK)
-async def updateAd(request: Request):
-    try:
-        body = await request.json()
-        message = {
-            "productId": body.get("productId"),
-            "ad": body.get("ad"),
-        }
-
-        for key, val in message.items():
-            if val is None:
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST, detail=f"{key} is required")
-
-        producer.send('updateAd', value=json.dumps(message).encode('utf-8'))
-        producer.flush()
-        return {'status': 'success', 'message': 'Tag updated successfully'}
+        if body.get('product_id') is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="product_id is required")
+        for topic, opts in kafka_map.items():
+            message = {key: body.get(key) for key in opts['fields'] if key in body}
+            if not message:
+                continue
+            message['product_id'] = body.get('product_id')
+            if opts['handler'] is not None:
+                message['main_image'] = opts['handler'](message)
+            producer.send(topic, value=json.dumps(message).encode('utf-8'))
+            producer.flush()
+        return {'status': 'success', 'message': 'Update successful'}
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
